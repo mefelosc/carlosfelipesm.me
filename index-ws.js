ee0945c54df9622718ae77ccfedc50e4e4b7a97a
Carlos Felipe Silva Melo <39221271+mefelosc@users.noreply.github.com>
2025-11-11 11:02:17 -0300

    Remove node_modules from tracking

--- index-ws.js
+++ index-ws.js
@@ -8,3 +8,32 @@ app.get('/', function(req, res) {
 
     server.on('request', app);
     server.listen(3000, function () { console.log('Listening on 3000'); });
+
+/** Websocket */
+
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({server: server});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+};
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
+
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server');
+    }
+
+    ws.on('close', function close() {
+        console.log('A client has disconnected');
+        wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    });
+
+});
